6c87a8b7893c1d3597c51ca1400782addee4c3c8
Janki P. Parmar <108918275+76jankihere@users.noreply.github.com>
2025-05-13 06:13:25 +0000

    publish link added

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,11 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
+    base: '/Janki-portfolio-website/', 
+    server: {
     host: "::",
-    port: 8080,
-  },
+    port: 8080, 
+    },
   plugins: [
     react(),
     mode === 'development' &&
