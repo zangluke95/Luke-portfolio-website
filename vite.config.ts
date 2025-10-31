c772ab41f270e778288a8866d12d6972f3b69ee7
Luke Zang <zangluke95@gmail.com>
2025-10-30 22:48:49 -0400

    Fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-    base: '/Luke-portfolio-website/', 
+    base: './', 
     server: {
     host: "::",
     port: 8080, 
